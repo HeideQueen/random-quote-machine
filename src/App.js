3067ab921a0970845f2987c04808c16de1eda427
HeideQueen <lucyfabz@gmail.com>
2019-12-22 00:40:54 -0600

    API Fix and refactor.
    switch API call for one that uses HTTPS and refactor some of the codebase.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import QuoteBox from './components/quote-box/quote-box.component';
 import './App.scss';
 
 const App = () => {
-  const [quote, setQuote] = useState({});
+  const [newQuote, setNewQuote] = useState([]);
 
   const getNewQuote = () => {
-    fetch('http://quotes.stormconsultancy.co.uk/random.json')
+    fetch(
+      'https://www.abbreviations.com/services/v2/quotes.php?uid=7073&tokenid=akivxDtjkVeBtEZi&searchtype=RANDOM&format=json'
+    )
       .then(res => res.json())
-      .then(newQuote => setQuote(newQuote));
+      .then(newQuote => setNewQuote([newQuote]))
+      .catch(error => console.log(error));
   };
 
   useEffect(() => {
     getNewQuote();
   }, []);
 
-  return (
-    <div id='app'>
-      <QuoteBox quote={quote} getNewQuote={getNewQuote} />
-    </div>
-  );
+  if (newQuote.length) {
+    const { author, quote } = newQuote[0].result;
+
+    return (
+      <div id="app">
+        <QuoteBox author={author} quote={quote} getNewQuote={getNewQuote} />
+      </div>
+    );
+  } else {
+    return <div>...loading</div>;
+  }
 };
 
 export default App;
